b8d98418dba0a8b16e158d1a36cdfa72c5ed1a03
Matt Etress <mattetress@me.com>
2018-10-10 00:18:20 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,12 +7,16 @@ class School
   end
 
   def add_student(name, grade)
-    @roster[grade] ||= [] 
+    @roster[grade] ||= []
     @roster[grade] << name
   end
 
   def grade(grade)
     @roster[grade]
+  end
+
+  def sort
+    @roster.sort
   end
 
 end
